27cb172720ef5a37e4331d11ffc59747ff391fbd
bbao <bbao>
2017-04-20 23:09:13 +0800

    add the String

---
+++ BaseForm/AddString.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+first = '上海涌洁'
+second = ' 我的第二个公司'
+third = first + second
+print third
+
+
+print 5 + 8
+print '5' + '8'
+
+
+print 'let\'t go'
+print "let't go"
+
+
+print 'c:\now'
+print 'c:\\now'
+
+
+# 原始字符串
+print '***********************************'
+
+fish = r'c:\now'
+# 注意一下，fish = r'c:\now\' 这样是不合法的
+print fish
+
+
+# 多行字符串 它能处理一段东西
+hope = """
+阳光总在风雨后
+可是我总在面对风雨
+
+我不知道未来是什么
+我能做的
+只有的往前走
+"""
+print hope
+
+
+
+
+
