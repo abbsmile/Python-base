d88ca01c5edcf7b19f0d59e29d85009386644980
abbsmile <17317591875@163.com>
2017-04-25 08:15:20 +0800

    20170425

---
+++ BaseForm/time.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+# import time
+# import Calendar
+
+import datetime
+
+# localtime = time.localtime(time.time())
+# print "本地时间为 :", localtime
+
+
+
+# 比较没有什么意义，这个不是时间
+# print 2013-03-04 > 2013-03-05  # 早的大，成立
+# print 2013-03-04 > 2013-03-02
+#
+#
+# print 2013-02-04 > 2013-03-04  # 成立
+# print 2013-02-02 > 2013-03-04  # 成立
+# print 2013-02-05 > 2013-03-04
+
+# print datetime.datetime.now().strftime("%Y-%m-%d")
+# print (datetime.datetime.now() - datetime.timedelta(days=100)).strftime("%Y-%m-%d")
+
+# (datetime.datetime.now() - datetime.timedelta(days = 100)).strftime("%Y-%m-%d")
